4ed47dfa1e8b640c5b881122ffbfd5d57feaa558
Alex Cline <rev.a.r.cline@gmail.com>
2020-09-16 06:15:55 -0500

    updated static pages route test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
   root 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
   get '/help',    to: 'static_pages#help'
