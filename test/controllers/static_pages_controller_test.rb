a3e033c3c848dea825b762fbc8cbbaf74f0262ef
Alex Cline <rev.a.r.cline@gmail.com>
2020-09-16 07:06:58 -0500

    refactored title provider, test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,30 +2,30 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @base_title = '| reflectotron'
+    @base_title = 'reflectotron'
   end
 
   test 'should get home' do
     get root_path
     assert_response :success
-    assert_select 'title', "home #{@base_title}"
+    assert_select 'title', @base_title
   end
 
   test 'should get help' do
     get help_path
     assert_response :success
-    assert_select 'title', "help #{@base_title}"
+    assert_select 'title', "help | #{@base_title}"
   end
 
   test 'should get contact' do
     get contact_path
     assert_response :success
-    assert_select 'title', "contact #{@base_title}"
+    assert_select 'title', "contact | #{@base_title}"
   end
 
   test 'should get about' do
     get about_path
     assert_response :success
-    assert_select 'title', "about #{@base_title}"
+    assert_select 'title', "about | #{@base_title}"
   end
 end
