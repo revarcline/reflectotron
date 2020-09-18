99eef8038bcebbcae9d117d113538f700c1f4efc
Alex Cline <rev.a.r.cline@gmail.com>
2020-09-18 12:41:53 -0500

    users controller and beginning of show

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#home'
+  get '/contact', to: 'static_pages#contact'
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get 'users/new'
+
+  resources :users
+
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
   devise_scope :user do
     get 'sign_in', to: 'users/sessions#new'
     get 'sign_up', to: 'users/registrations#new'
     get 'forgot_password', to: 'users/passwords#new'
     get 'reset_password', to: 'users/passwords#edit'
   end
-
-  root to: 'static_pages#home'
-  get '/contact', to: 'static_pages#contact'
-  get '/help',    to: 'static_pages#help'
-  get '/about',   to: 'static_pages#about'
 end
