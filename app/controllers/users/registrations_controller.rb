fbdea377a3ba572f84d19256db7e62f0c101a395
Alex Cline <rev.a.r.cline@gmail.com>
2020-09-22 15:36:33 -0500

    added jquery

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -48,6 +48,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
                                  :password_confirmation)
   end
 
+  def account_update_params
+    params.require(:user).permit(:username,
+                                 :fullname,
+                                 :email,
+                                 :password,
+                                 :password_confirmation)
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
